39b52268ac4a8f68d25acc6437883b2eb39661bd
Kararasenok =) <murzikkurzikpro@gmail.com>
2024-07-28 21:50:33 +0300

    v0.1b

--- build.bat
+++ build.bat
@@ -1,2 +1,18 @@
-pyinstaller --noconfirm --onefile --console --name "bbrweb" "D:\Desktop\НеПапка\bbrweb\main.py"
+@echo off
+echo "What do you want to do?"
+echo "1 = Remove file"
+echo "2"
+set /p answer=
+if "%answer%"=="y" (
+    del bbrweb.spec
+    echo "bbrweb.spec removed"
+    rmdir dist /s /q
+    echo "dist removed"
+    rmdir build /s /q
+    echo "build removed"
+    echo "=============================="
+    echo "Removed!"
+)
+
+python -m PyInstaller --noconfirm --onefile --console --name "bbrweb" "D:\Desktop\bbrweb\main.py"
 pause
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import os, requests, json
 
-input("Press Enter to continue...")
+domain_url = "https://raw.githubusercontent.com/kararasenok-gd/bbrweb/main/domains.json"
+cached_sites = requests.get(domain_url).json()
+
+for i in cached_sites.get("domains"):
+    name = i["name"]
+    tld = i["tld"]
+
+    full_url = name + "." + tld
+    print(full_url)
+
+x = input("url: ")
 
