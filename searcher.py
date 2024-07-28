4d4f11e8d2907ea359d672dd05971e423dc7c285
Kararasenok =) <murzikkurzikpro@gmail.com>
2024-07-28 23:24:37 +0300

    upd

--- searcher.py
+++ searcher.py
@@ -1,5 +1,4 @@
 import requests, json, argparse
-
 parser = argparse.ArgumentParser()
 # parser.add_argument("search")
 
@@ -9,6 +8,9 @@ url_domains = "https://raw.githubusercontent.com/kararasenok-gd/bbrweb/main/doma
 
 domains = json.loads(requests.get(url_domains).text)
 
-for domain in domains:
-    if tag in domain["tags"]:
-        print(domain["domain"])
+for domain in domains.get("domains", []):
+    # Find tag in domain
+    if tag in domain.get("tags", []):
+        print(str(domain.get("name")) + "." +  str(domain.get("tld")))
+        
+
